450f6445857875759b7ca9ec448be7529d0f9dc0
hqpko <whaibin01@hotmail.com>
2020-08-06 10:56:51 +0800

    fix waitTimeout

--- wait_timeout.go
+++ wait_timeout.go
@@ -7,16 +7,17 @@ import (
 
 // WaitTimeout 带有超时的 WaitGroup，避免了 wg.Wait() 可能引起的 blocked forever，协程永远无法被释放
 type WaitTimeout struct {
-	done  chan struct{}
-	count int32
+	doneChan chan struct{}
+	count    int32
 }
 
 // NewWaitTimeout 必须使用 NewWaitTimeout 实例化 WaitTimeout
 func NewWaitTimeout() *WaitTimeout {
-	return &WaitTimeout{done: make(chan struct{})}
+	return &WaitTimeout{doneChan: make(chan struct{})}
 }
 
 // Wait, 等待过程外置，外部使用时，可以先获取 chan, 在释放完可能存在的锁后，进入 <-chan 环节，避免等待时间内持有锁
+// 一个 WaitTimeout 只能使用一次，wt.Done() 后会 close(wt.doneChan)，再次 wt.Done() 会触发 panic
 func (wt *WaitTimeout) Wait(timeout time.Duration) chan bool {
 	c := make(chan bool, 1)
 	if count := atomic.LoadInt32(&wt.count); count <= 0 {
@@ -29,7 +30,7 @@ func (wt *WaitTimeout) Wait(timeout time.Duration) chan bool {
 
 func (wt *WaitTimeout) wait(timeout time.Duration, c chan bool) {
 	select {
-	case <-wt.done:
+	case <-wt.doneChan:
 		c <- true
 	case <-time.After(timeout):
 		c <- false
@@ -43,10 +44,7 @@ func (wt *WaitTimeout) Add(n int32) *WaitTimeout {
 
 func (wt *WaitTimeout) Done() {
 	if count := atomic.AddInt32(&wt.count, -1); count <= 0 {
-		select {
-		case wt.done <- struct{}{}:
-		default:
-		}
+		close(wt.doneChan)
 	}
 }
 
