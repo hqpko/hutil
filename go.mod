d4284e78ef3f2f4c755b542e16b4c4308e2282c3
hqpko <whaibin01@126.com>
2024-06-06 14:23:12 +0800

    feat: rand slice/map n

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/hqpko/hutils
 
-go 1.14
+go 1.18
--- rand.go
+++ rand.go
@@ -1,6 +1,7 @@
 package hutils
 
 import (
+	"cmp"
 	"math/rand/v2"
 )
 
@@ -14,4 +15,27 @@ func RandString(n int) string {
 		buf[i] = strBase[rand.IntN(strMaxLen)]
 	}
 	return string(buf)
+}
+
+func RandSlice[T any](slice []T, n int) []T {
+	l := len(slice)
+	if n > l {
+		n = l
+	}
+	r := make([]T, n)
+	for i := 0; i < n; i++ {
+		j := rand.IntN(l)
+		r[i] = slice[j]
+		slice[j], slice[l-1] = slice[l-1], slice[j]
+		l--
+	}
+	return r
+}
+
+func RandMap[T cmp.Ordered, U any](m map[T]U, n int) []T {
+	keys := make([]T, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	return RandSlice(keys, n)
 }
