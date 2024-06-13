2aaed4664c449f77ee4f22112724e3fe6b5efa85
hqpko <whaibin01@126.com>
2024-06-13 16:12:37 +0800

    feat: RandSliceWeightFunc,RandSliceWeightFuncN

--- rand.go
+++ rand.go
@@ -2,6 +2,7 @@ package hutils
 
 import (
 	"math/rand/v2"
+	"sort"
 )
 
 const strBase = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -45,4 +46,46 @@ func RandMapV[K comparable, V any](m map[K]V, n int) []V {
 		values = append(values, v)
 	}
 	return RandSlice(values, n)
+}
+
+func RandSliceWeightFunc[T any](slice []T, weightFunc func(T) int) T {
+	if len(slice) == 0 {
+		var zero T
+		return zero
+	}
+
+	prefixSums := make([]int, len(slice))
+	totalWeight := 0
+	for i, item := range slice {
+		totalWeight += weightFunc(item)
+		prefixSums[i] = totalWeight
+	}
+	rnd := rand.IntN(totalWeight)
+	idx := sort.Search(len(prefixSums), func(j int) bool {
+		return prefixSums[j] > rnd
+	})
+	return slice[idx]
+}
+
+// RandSliceWeightFuncN 根据权重随机选择 n 个元素，权重由 weightFunc 提供，可以重复选择同一个元素
+func RandSliceWeightFuncN[T any](slice []T, n int, weightFunc func(T) int) []T {
+	if n <= 0 || len(slice) == 0 {
+		return nil
+	}
+
+	prefixSums := make([]int, len(slice))
+	totalWeight := 0
+	for i, item := range slice {
+		totalWeight += weightFunc(item)
+		prefixSums[i] = totalWeight
+	}
+	selectedItems := make([]T, 0, n)
+	for i := 0; i < n; i++ {
+		rnd := rand.IntN(totalWeight)
+		idx := sort.Search(len(prefixSums), func(j int) bool {
+			return prefixSums[j] > rnd
+		})
+		selectedItems = append(selectedItems, slice[idx])
+	}
+	return selectedItems
 }
